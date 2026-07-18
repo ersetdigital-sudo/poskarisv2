18107706da260df5607cc258bab57f33f8d63877
ersetdigital-sudo <ersetdigital-sudo@users.noreply.github.com>
2026-07-18 19:54:32 +0700

    fix: set turbopack root to avoid multiple lockfile conflict

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: "C:/Users/user/Downloads/kasir-pos-laptop",
+  },
 };
 
 export default nextConfig;
